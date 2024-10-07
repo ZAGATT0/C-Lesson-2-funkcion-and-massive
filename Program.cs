992d393a14ea1be0edc1b10267a4334d80e7186d
Zagatto <zagatto@mail.ru>
2024-10-07 13:33:01 +0300

    то же решение, но черезю функция в функции

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ int Max (int arg1, int arg2, int arg3)
 
 
 int a1 = 15;
-int b1 = 21;
+int b1 = 2541;
 int c1 = 39;
 int a2 = 122;
 int b2 = 23;
@@ -18,9 +18,14 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max (a1, b1, c1);
-int max2 = Max (a2, b2, c2);
-int max3 = Max (a3, b3, c3);
-int max = Max (max1, max2, max3);
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
+
+int max = Max (
+    Max (a1, b1, c1),
+    Max (a2, b2, c2), 
+    Max (a3, b3, c3));
 
 Console.WriteLine(max);
